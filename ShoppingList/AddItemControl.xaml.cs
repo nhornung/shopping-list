193835a67359d449251d442ff309a81734aef724
nick hornung <nphornung77@gmail.com>
2020-02-24 17:13:17 -0600

    in class work

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+
+            AddItemButton.Click += OnAddItemButtonClick;
+        }
+
+        protected void OnAddItemButtonClick(object sender, RoutedEventArgs args)
+        {
+            /*
+            var item = ItemToAdd.Text;
+
+            //var data = (ShoppingListData)DataContext; this throws an exception if does not work 
+            var data = DataContext as ShoppingListData; // This one assigns data null if it doesnt work
+
+            if (data !=null)
+            {
+                // safe to work with data
+            }
+
+            if (DataContext is ShoppingListData data2) //data2 will only exist in this block and will be a shopping list data
+            {
+
+            }
+            */
+
+            if(DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ShoppingList
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    {
+        //This controls how the list is changed. A safe and secure way (want to manipulate data in our choice of interface)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<string> items = new List<string>();
+
+        public IEnumerable<string> Items => items.ToArray(); //makes a copy, hides data with the toarray
+
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items")); //must spell string of thing your changing exactly same
+        }
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+    }
+}
